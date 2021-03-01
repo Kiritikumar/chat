e63ab1f7f53ea839004a14fb9240d0fa68e3edea
Kiritikumar <77089907+Kiritikumar@users.noreply.github.com>
2021-03-01 10:06:19 +0530

    added to chat app

---
+++ ChatAssignmentNityaSoft/src/main/java/com/chatApp/ChatAssignmentNityaSoftApplication.java
@@ -0,0 +1,13 @@
+package com.chatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAssignmentNityaSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAssignmentNityaSoftApplication.class, args);
+	}
+
+}
